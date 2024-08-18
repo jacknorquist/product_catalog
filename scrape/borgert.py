191bb7ba93d8323b05362bfa2aba2321862c5130
Jack Norquist <jack.norquist@outlook.com>
2024-08-18 12:29:02 -0500

    get product links

--- scrape/borgert.py
+++ scrape/borgert.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from urllib.parse import urljoin
 import time
 import sys
@@ -30,10 +31,17 @@ def get_product_links(catalog_url):
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.get(catalog_url)
+    driver.maximize_window()
+
 
     # Use WebDriverWait to wait for the page to fully load
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.sub-menu')))
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.menu-item-type-custom')))
+    time.sleep(3)
+
+    products_header = driver.find_element(By.CSS_SELECTOR, '.b-products')
+    product_menu = products_header.find_element(By.CSS_SELECTOR, '#menu-products')
+
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
@@ -41,24 +49,17 @@ def get_product_links(catalog_url):
     # List to store product links with their associated categories
     product_info = []
 
-    # Iterate over each .sub-menu element
-    for sub_menu in soup.select('.sub-menu'):
-        # Find the sibling <a> element that contains the category
-        category_element = sub_menu.find_previous_sibling('a')
-        if category_element:
-            category_text = category_element.get_text(strip=True)
-        else:
-            category_text = 'Unknown'
+    menu_items = product_menu.find_elements(By.CSS_SELECTOR, '.menu-item-type-custom')
 
-        # Iterate over each .menu-item.menu-item-type-post_type within this sub-menu
-        for item in sub_menu.select('.menu-item.menu-item-type-post_type'):
-            link_element = item.find('a', href=True)
-            if link_element:
-                product_link = link_element['href']
-                if product_link = 'https://www.borgertproducts.com/fireplaces-ovens-fire-rings/':
-                    return
-                else:
-                    product_info.append((product_link, category_text))
+    # Iterate over each .sub-menu element
+    for product_item in menu_items:
+        actions = ActionChains(driver)
+        actions.move_to_element(product_item).perform()
+        # Find the sibling <a> element that contains the category
+        category_text = product_item.find_element(By.TAG_NAME, 'a').text.strip()
+        for link_div in product_item.find_elements(By.CSS_SELECTOR, '.menu-item-type-post_type'):
+            product_link = link_div.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            product_info.append((product_link, category_text))
 
     driver.quit()
     return product_info
@@ -95,11 +96,11 @@ def get_product_details(product_url, category):
     vc_items = soup.find_all(class_='vc_item')
     for item in vc_items:
     # Find the image within this vc_item
-    img_tag = item.find('img')
-    if img_tag and 'src' in img_tag.attrs:
-        img_url = img_tag['src']
-        # Join the URL with the base URL
-        image_urls.append(img_url)
+        img_tag = item.find('img')
+        if img_tag and 'src' in img_tag.attrs:
+            img_url = img_tag['src']
+            # Join the URL with the base URL
+            image_urls.append(img_url)
 
     s3_main_images = [upload_image_stream_to_s3(img_url, s3_bucket_name, f"borgert/{product_details['name']}/images/main_{i}.jpg") for i, img_url in enumerate(image_urls)]
     product_details['images'] = s3_main_images
@@ -108,20 +109,20 @@ def get_product_details(product_url, category):
     ##size
     size_entries=[]
     size_items = driver.find_elements(By.CSS_SELECTOR,'.vc_clearfix')
-        for size in size_item:
-            h4_text = size.find_element(By.TAG_NAME, 'h4').text.strip()
-            lines = h4_text.split('\n')
-            name = lines[0]
-            dimensions = lines[1]
-            image_url = size.find_element(By.CSS_SELECTOR, '.vc_gitem-zone-img').get_attribute('src')
-            s3_size_image_url = upload_image_stream_to_s3(img_url, s3_bucket_name, f"borgert/{product_details['name']}/sizes/{name}.png")
+    for size in size_item:
+        h4_text = size.find_element(By.TAG_NAME, 'h4').text.strip()
+        lines = h4_text.split('\n')
+        name = lines[0]
+        dimensions = lines[1]
+        image_url = size.find_element(By.CSS_SELECTOR, '.vc_gitem-zone-img').get_attribute('src')
+        s3_size_image_url = upload_image_stream_to_s3(img_url, s3_bucket_name, f"borgert/{product_details['name']}/sizes/{name}.png")
 
-            size_entry = {
-                'name': name,
-                'image': s3_size_img_url,
-                'dimensions': dimensions
-            }
-            size_entries.append(size_entry)
+        size_entry = {
+            'name': name,
+            'image': s3_size_img_url,
+            'dimensions': dimensions
+        }
+        size_entries.append(size_entry)
 
     product_details['sizes']=size_entries
 

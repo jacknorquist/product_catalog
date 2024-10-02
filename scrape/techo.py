16bcae6de0202ba6faf21ab68ef69078a418d65e
Jack Norquist <jack.norquist@outlook.com>
2024-10-01 19:16:41 -0500

    updated iframe class detection

--- scrape/techo.py
+++ scrape/techo.py
@@ -117,7 +117,7 @@ def get_product_details(product_url):
         )
         driver.switch_to.frame(iframe)
         assist_close = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, '.VizExIconButton__AbstractVizExIconButton-sc-1rptov-0 '))
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '.VizExIconButton__AbstractVizExIconButton-sc-1rgioci-0'))
         )
         assist_close.click();
         driver.switch_to.default_content()
